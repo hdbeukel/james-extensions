bf1774f1ab0b5b7a19be9de5055ea9af5744c30a
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2015-04-09 09:06:20 +0200

    removed unused imports

--- src/main/java/org/jamesframework/ext/analysis/Analysis.java
+++ src/main/java/org/jamesframework/ext/analysis/Analysis.java
@@ -16,9 +16,7 @@
 
 package org.jamesframework.ext.analysis;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.jamesframework.core.exceptions.JamesRuntimeException;
 import org.jamesframework.core.problems.Problem;
